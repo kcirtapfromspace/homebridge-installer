a2f7328128079adfcb722d9fd63de273dc791214
Patrick Deutsch <105461352+kcirtapfromspace@users.noreply.github.com>
2023-11-30 20:58:11 -0700

    updates

--- homebridge_docker_install.sh
+++ homebridge_docker_install.sh
@@ -10,19 +10,7 @@ if [ ! -d "$path2" ]; then
         mkdir $path2
 fi
 
-curl https://raw.githubusercontent.com/mbierman/homebridge-installer/main/docker-compose.yaml > $path2/docker-compose.yaml
-
-echo "What is your timezone? (see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)"
-read < /dev/tty -p "Enter your timezone and press [ENTER]: $TZ " TZ && TZ=${TZ:-America/Los_Angeles}
-printf "\n\n"
-sed "s|TZ.*|TZ=${TZ}|g" $path2/docker-compose.yaml > $path2/docker-compose.yaml.tmp
-mv $path2/docker-compose.yaml.tmp $path2/docker-compose.yaml
-
-read < /dev/tty -p "Ener the port you want to run homebridge on and press [ENTER] (8080 is the default):  $port " port && port=${port:-8080}
-printf "\n"
-sed "s|HOMEBRIDGE_CONFIG_UI_PORT.*|HOMEBRIDGE_CONFIG_UI_PORT=${port}|g" $path2/docker-compose.yaml > $path2/docker-compose.yaml.tmp
-mv $path2/docker-compose.yaml.tmp $path2/docker-compose.yaml
-printf "\n\n"
+curl https://raw.githubusercontent.com/kcirtapfromspace/homebridge-installer/main/docker-compose.yaml > $path2/docker-compose.yaml
 
 
 cd $path2
